93ce4b6139a31cfba2d7abfccd1356e6250e80dd
mario <mario@mario-Z450UAK>
2017-11-19 17:26:59 -0200

    Criação da estrutura basica para o deploy

--- src/main/java/br/com/uniplus/materialmanager/MaterialManagerApplication.java
+++ src/main/java/br/com/uniplus/materialmanager/MaterialManagerApplication.java
@@ -2,12 +2,24 @@ package br.com.uniplus.materialmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class MaterialManagerApplication {
+@PropertySources({ 
+	    @PropertySource("classpath:application.properties"),
+		@PropertySource(value = "file:${external.config}") })
+public class MaterialManagerApplication extends SpringBootServletInitializer{
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MaterialManagerApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MaterialManagerApplication.class, args);
 	}

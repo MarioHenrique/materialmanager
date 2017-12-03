c32f046ad0de31843e002eb19d960215f0677779
mario <mario@mario-Z450UAK>
2017-12-03 21:26:07 -0200

    Ajuste da navegação entre as paginas

--- src/main/java/br/com/uniplus/materialmanager/services/UserService.java
+++ src/main/java/br/com/uniplus/materialmanager/services/UserService.java
@@ -1,12 +1,16 @@
 package br.com.uniplus.materialmanager.services;
 
+import java.util.HashSet;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.uniplus.materialmanager.dto.request.RegisterRequestDTO;
+import br.com.uniplus.materialmanager.entities.Role;
 import br.com.uniplus.materialmanager.entities.User;
 import br.com.uniplus.materialmanager.exception.UserAlreadyExistException;
+import br.com.uniplus.materialmanager.repository.RoleRepository;
 import br.com.uniplus.materialmanager.repository.UserRepository;
 
 @Service
@@ -14,6 +18,9 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
 	
 	@Autowired
 	private BCryptPasswordEncoder encoder;
@@ -29,6 +36,18 @@ public class UserService {
 		newUser.setName(userRequested.getName());
 		newUser.setUsername(userRequested.getUsername());
 		newUser.setPassword(encoder.encode(userRequested.getPassword()));
+		
+		Role role = roleRepository.findByName(userRequested.getProfile().toString());
+		if(role == null) {
+			role = new Role();
+			role.setName(userRequested.getProfile().toString());
+			roleRepository.save(role);
+		}
+		
+		HashSet<Role> roles = new HashSet<>();
+		roles.add(role);
+		
+		newUser.setRoles(roles);
 		
 		userRepository.save(newUser);
 	}
